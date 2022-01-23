fd0a57b7bd29b7292fe921ff7be44cad701e0b92
Abdullah Al Tansen <abdullahaltansen899@gmail.com>
2022-01-24 00:36:28 +0600

    updated full work

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAsjcNcdU7JBV-P5q1sFPN5EvnXUQd8sik",
+    authDomain: "react-firebase-authentic-fbd5d.firebaseapp.com",
+    projectId: "react-firebase-authentic-fbd5d",
+    storageBucket: "react-firebase-authentic-fbd5d.appspot.com",
+    messagingSenderId: "732571843080",
+    appId: "1:732571843080:web:b4e68c94d84c8a6b59afbd"
+  };
+export default firebaseConfig;  
